7feb35251c03c79411079903d94270e72e7af215
manojkumar-0108 <manoj8894421801@gmail.com>
2024-12-03 07:09:05 +0530

    Fix: modified App to show only latest code

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import "./App.css";
 
 import Card from "./components/Card";
-
-import Login from "./components/nav-bar/Login";
+import CounterPage from "./pages/counter/CounterPage";
+import Search from "./pages/search-filter/Search";
 
 export default function App() {
   console.log(" ");
 
   return (
     <div className="main-app">
-      {/* <Card/> */}
-      {/* <h1>Hello world</h1> */}
-      {/* <Test /> */}
-      <Login />
+      {/* <CounterPage /> */}
+      <Search />
     </div>
   );
 }
